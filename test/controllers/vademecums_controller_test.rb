4835c287cdf2cd07c5ba0f1df05088da1ee835ca
Nicolás Galdámez <nicogaldamez@gmail.com>
2015-11-20 17:37:44 -0300

    Vademecum controller test

--- test/controllers/vademecums_controller_test.rb
+++ test/controllers/vademecums_controller_test.rb
@@ -1,7 +1,55 @@
 require 'test_helper'
 
 class VademecumsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include Sorcery::TestHelpers::Rails::Integration
+  include Sorcery::TestHelpers::Rails::Controller
+  
+  setup do
+    @vademecum = vademecums(:one)
+    @product1 = products(:one)
+    @product2 = products(:two)
+    
+    @user = users(:god)
+    
+    login_user(user = @user, route=login_url)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:vademecums)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create vademecum" do
+    # TODO Products discount association
+    assert_difference('Vademecum.count') do
+      post :create, vademecum: { name: @vademecum.name }
+    end
+
+    assert_redirected_to vademecums_path
+  end
+
+  test "should get edit" do
+    get :edit, id: @vademecum
+    assert_response :success
+  end
+
+  test "should update vademecum" do
+    # TODO Products discount association
+    patch :update, id: @vademecum, vademecum: { name: @vademecum.name }
+    assert_redirected_to vademecums_path
+  end
+
+  test "should destroy vademecum" do
+    assert_difference('Vademecum.count', -1) do
+      delete :destroy, id: @vademecum
+    end
+
+    assert_redirected_to vademecums_path
+  end
 end
