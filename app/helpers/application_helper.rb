6b40935c2ed5340454b051aaaa14701137435409
Francisco Peña <francisco@snappler.com>
2015-12-17 16:48:04 -0300

    minos changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
     #klass = 'danger'  if klass == 'alert'
     flash.discard(klass)
     content_tag :span, '', class: 'snackbar-message',
-      data: {toggle: :snackbar, content: message, timeout: 10000}
+                data: {toggle: :snackbar, content: message, timeout: 10000}
   end
 
   def flash_messages
@@ -24,7 +24,7 @@ module ApplicationHelper
     html_options[:class] = ['fa', "fa-#{name}", html_options[:class]].compact
     content_tag(:i, nil, html_options)
   end
-  
+
   # Link del menú lateral
   def side_link(link_text, link_path, icon_class='', klass='', html_options={}, extra='')
     link_class = html_options[:class]
@@ -34,7 +34,7 @@ module ApplicationHelper
       (link_to link_path, html_options do
         "<i class='fa fa-fw fa-#{icon_class}'></i> <span> #{link_text} </span>".html_safe
       end) +
-      extra.html_safe
+          extra.html_safe
     end
   end
 
@@ -53,14 +53,14 @@ module ApplicationHelper
   def seller?
     logged_in? && current_user.role == 'seller' && current_user.supplier_id
   end
-  
+
   def admin?
     logged_in? && current_user.role == 'admin' && current_user.supplier_id
   end
-  
+
   def normal_user?
     logged_in? && current_user.role == 'normal_user'
-  end    
+  end
 
   def is_me?(user)
     logged_in? && current_user == user
@@ -73,7 +73,7 @@ module ApplicationHelper
 
   def reward_order_state(reward_order)
     actions = reward_order.get_state_actions
-    actions.collect{|x| link_to t(x, scope: :reward_order_states), change_state_reward_order_path(reward_order, state: x), remote: true, class: "btn btn-xs #{(['canceled','not_delivered'].include?(x))? 'btn-danger' : 'btn-success'}" }.join().html_safe
+    actions.collect { |x| link_to t(x, scope: :reward_order_states), change_state_reward_order_path(reward_order, state: x), remote: true, class: "btn btn-xs #{(['canceled', 'not_delivered'].include?(x)) ? 'btn-danger' : 'btn-success'}" }.join(' ').html_safe
   end
 
 end
