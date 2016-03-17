7bbc6393b603a15da0ccb3461aa4ac135c5c7669
Francisco Peña <francisco@snappler.com>
2016-03-17 14:16:41 -0300

    select2 images

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ gem 'mysql2', '~> 0.3.20'
 
 gem 'unicorn'
 
-# gem 'sass-rails'
+gem 'sass-rails'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'therubyracer', platforms: :ruby
--- config/deploy.rb
+++ config/deploy.rb
@@ -90,11 +90,6 @@
      invoke :'rails:assets_precompile'
      invoke :'deploy:cleanup'
 
-     # invoke :'unicorn:stop'
-     # invoke :'unicorn:start'
-     queue "sudo /etc/init.d/unicorn_manes_#{rails_env} stop"
-     queue "sudo /etc/init.d/unicorn_manes_#{rails_env} start"
-
      # to :launch do
      #   queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
      #   queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
