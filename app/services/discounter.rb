d1659985cf61dd794f4e3026e8fd2369555521a7
GONZALO <gonzagaldamez@gmail.com>
2016-04-13 16:07:13 -0300

    Corrección en descuentos

--- app/services/discounter.rb
+++ app/services/discounter.rb
@@ -46,16 +46,18 @@ class Discounter
 
   def corresponding_discount(product_discounts)
     collection = product_discounts.select { |pd| pd.health_insurance_id == @health_insurance_id && pd.coinsurance_id == @coinsurance_id }
-    if collection.empty? #Chequear por OS o por COS
+    if (collection.empty? || !health_insurance_and_coinsurance?) #Chequear por OS o por COS
       hi_collection = product_discounts.select { |pd| pd.health_insurance_id == @health_insurance_id }
       co_collection = product_discounts.select { |pd| pd.coinsurance_id == @coinsurance_id }
-      if hi_collection.empty? && co_collection.empty?
-        discount = product_discounts.collect { |pd| pd.discount }.max
+     
+      if ((hi_collection.empty? && co_collection.empty?) || !health_insurance_and_coinsurance?)
+        discount = product_discounts.collect { |pd| pd.discount }.max # Descuento en efectivo
       else #Descuento Obra Social o Descuento Coseguro
         hi_discount = hi_collection.collect { |pd| pd.health_insurance_discount }.max
         co_discount = co_collection.collect { |pd| pd.coinsurance_discount }.max
         discount = [hi_discount, co_discount].compact.max
       end
+      
     else #Descuento OS + COS
       discount = collection.collect { |pd| pd.health_insurance_and_coinsurance_discount }.max
     end  
@@ -63,5 +65,8 @@ class Discounter
     discount
     
   end
-
+  
+  def health_insurance_and_coinsurance? 
+    @health_insurance_id.present? && @coinsurance_id.present?
+  end
 end
