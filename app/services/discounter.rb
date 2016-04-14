dd110a06c1ae9d86cb4a549ceaff81caf4d22944
GONZALO <gonzagaldamez@gmail.com>
2016-04-14 12:10:08 -0300

    Cambios en descuentos

--- app/services/discounter.rb
+++ app/services/discounter.rb
@@ -45,28 +45,49 @@ class Discounter
   end
 
   def corresponding_discount(product_discounts)
-    collection = product_discounts.select { |pd| pd.health_insurance_id == @health_insurance_id && pd.coinsurance_id == @coinsurance_id }
-    if (collection.empty? || !health_insurance_and_coinsurance?) #Chequear por OS o por COS
-      hi_collection = product_discounts.select { |pd| pd.health_insurance_id == @health_insurance_id }
-      co_collection = product_discounts.select { |pd| pd.coinsurance_id == @coinsurance_id }
-     
-      if ((hi_collection.empty? && co_collection.empty?) || !health_insurance_and_coinsurance?)
-        discount = product_discounts.collect { |pd| pd.discount }.max # Descuento en efectivo
-      else #Descuento Obra Social o Descuento Coseguro
-        hi_discount = hi_collection.collect { |pd| pd.health_insurance_discount }.max
-        co_discount = co_collection.collect { |pd| pd.coinsurance_discount }.max
-        discount = [hi_discount, co_discount].compact.max
-      end
-      
-    else #Descuento OS + COS
-      discount = collection.collect { |pd| pd.health_insurance_and_coinsurance_discount }.max
+    if not_health_insurance_and_coinsurance? 
+      #No hay OS ni CO
+      discount = product_discounts.collect { |pd| pd.discount }.max # Descuento en efectivo
+    else
+      collection = product_discounts.select { |pd| same_health_insurance?(pd.health_insurance_id) && same_coinsurance?(pd.coinsurance_id) }
+      if collection.empty? 
+        #Descuento Obra Social o Descuento Coseguro
+        hi_collection = product_discounts.select { |pd| same_health_insurance?(pd.health_insurance_id) }
+        co_collection = product_discounts.select { |pd| same_coinsurance?(pd.coinsurance_id) }
+        if hi_collection.empty? && co_collection.empty?
+          #El producto está pero sin OS y CO cargados
+          discount = product_discounts.collect { |pd| pd.discount }.max # Descuento en efectivo
+        else
+          #El producto tiene OS o CO
+          hi_discount = hi_collection.collect { |pd| pd.health_insurance_discount }.max
+          co_discount = co_collection.collect { |pd| pd.coinsurance_discount }.max
+          discount = [hi_discount, co_discount].compact.max          
+        end
+      else 
+        #Descuento OS + COS
+        discount = collection.collect { |pd| pd.health_insurance_and_coinsurance_discount }.max
+      end 
     end
-
+    
     discount
 
   end
   
-  def health_insurance_and_coinsurance? 
-    @health_insurance_id.present? && @coinsurance_id.present?
+  # def health_insurance_and_coinsurance?
+  #   @health_insurance_id.present? && @coinsurance_id.present?
+  # end
+  
+  def not_health_insurance_and_coinsurance? 
+    @health_insurance_id.nil? && @coinsurance_id.nil?
   end
+  
+  def same_health_insurance?(health_insurance_id)
+    (@health_insurance_id.present? && (health_insurance_id == @health_insurance_id))
+  end
+  
+  def same_coinsurance?(coinsurance_id)
+    (@coinsurance_id.present? && (coinsurance_id == @coinsurance_id))
+  end
+    
+  
 end
